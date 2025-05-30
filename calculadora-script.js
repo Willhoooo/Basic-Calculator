4c78b67d7fba0bd8ce751c0e00145bf270af5a2d
William Mattson Kheel Gomes Araújo <157336038+Willhoooo@users.noreply.github.com>
2025-05-30 13:35:47 -0400

    Correção de bug na operação de porcentagem
    Foi feita uma correção de um bug que acontecia na operação de porcentagem, pois dava valor que não coincidia.

--- calculadora-script.js
+++ calculadora-script.js
@@ -4,24 +4,45 @@ let buttons = document.querySelectorAll('button');
 let string = "";
 let arr = Array.from(buttons);
 arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            try {
+                let expressionToEvaluate = string;
 
-        else if(e.target.innerHTML == 'AC'){
+                expressionToEvaluate = expressionToEvaluate.replace(/(\d+(\.\d+)?)\s*([+\-*/])\s*(\d+(\.\d+)?)%/g, (match, p1, p2, operator, p3, p4) => {
+                    return `${p1} ${operator} (${p3}/100)`;
+                });
+
+                expressionToEvaluate = expressionToEvaluate.replace(/(\d+(\.\d+)?)%/g, (match, p1) => {
+                    return `(${p1}/100)`;
+                });
+
+                string = eval(expressionToEvaluate).toString();
+                input.value = string;
+            } catch (error) {
+                input.value = "Erro";
+                string = "";
+            }
+        } else if (e.target.innerHTML == 'AC') {
             string = "";
             input.value = string;
-        }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
+        } else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
             input.value = string;
         }
-        else{
+        else if (e.target.innerHTML == '%') {
+            const lastChar = string.slice(-1);
+            if (['+', '-', '*', '/', '%'].includes(lastChar)) {
+                input.value = "Erro: Sintaxe Inválida";
+                string = "";
+            } else {
+                string += e.target.innerHTML;
+                input.value = string;
+            }
+        }
+        else {
             string += e.target.innerHTML;
             input.value = string;
         }
-        
-    })
-})
+    });
+});
